1462f2e08c1f4220ceacd6ee73ee8ba533d49531
Elysia <elysiaEgo@outlook.com>
2023-12-21 09:13:36 +0800

    feat: add Docker compose & Spring Security

---
+++ web-voyager-ddd-bootstrap/src/main/java/io/github/sztuwebclub/webvoyager/bootstrap/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package io.github.sztuwebclub.webvoyager.bootstrap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                (authz) ->
+                        authz.anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults()).build();
+    }
+}
